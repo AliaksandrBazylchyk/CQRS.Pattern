53381f442bd0e8f0ab8e0bdb5a79e82e71a93fa5
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-03-31 16:50:08 +0300

    Created Person entity

---
+++ CQRS.Pattern/Models/Persons/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CQRS.Pattern.Models.Persons
+{
+    public class Person : BaseEntity
+    {
+        /// <summary>
+        /// Person's first name
+        /// </summary>
+        [BsonRequired]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Person's second name
+        /// </summary>
+        [BsonRequired]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Person's age
+        /// </summary>
+        [BsonRequired]
+        public uint Age { get; set; }
+    }
+}
