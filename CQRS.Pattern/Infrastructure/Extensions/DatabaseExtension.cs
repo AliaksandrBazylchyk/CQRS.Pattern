e4670da15855ba4d50af489b5c86888c91fba058
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-03-31 16:48:12 +0300

    Configured contexts for MongoDB and Postgres

---
+++ CQRS.Pattern/Infrastructure/Extensions/DatabaseExtension.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Pattern.Infrastructure.Configurations;
+using CQRS.Pattern.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRS.Pattern.Infrastructure.Extensions
+{
+    public static class DatabaseExtension
+    {
+        public static IServiceCollection AddDbCollection(
+            this IServiceCollection services,
+            ConnectionOptions connection
+        )
+        {
+            services.AddDbContext<NpgsqlContext>(s => { s.UseNpgsql(connection.PostgresConnectionString); });
+            services.AddSingleton<MongoDBContext>();
+            return services;
+        }
+    }
+}
